7f303d48130b67d76f644b408405718ac1665290
yar0316 <40818353+yar0316@users.noreply.github.com>
2025-03-23 11:10:28 +0900

    added authenticator

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -25,6 +25,11 @@ function createTodo() {
     listTodos();
   });
 }
+
+  
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id })
+}
     
 // fetch todos when the component is mounted
  onMounted(() => {
@@ -40,7 +45,8 @@ function createTodo() {
     <ul>
       <li 
         v-for="todo in todos" 
-        :key="todo.id">
+        :key="todo.id"
+        @click="deleteTodo(todo.id)">
         {{ todo.content }}
       </li>
     </ul>
